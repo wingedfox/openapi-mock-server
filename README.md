f3587432a8b62b103483d7d3ed25ef85df8c389f
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-04-06 20:48:22 +0200

    Initial revision

---
+++ README.md
@@ -0,0 +1,28 @@
+[![Build Status][travis_badge]][travis_link] [![js-standard-style][js_standard_badge]][js_standard_link]
+
+# openapi-mock-server
+
+- creates mock server based on Open API 3.0 specification
+
+## Install
+
+```bash
+$ pnpm i -g openapi-mock-server
+$
+```
+
+## How to use it?
+
+```bash
+$ openapi-mock-server --api=yourapi.yml
+OpenApiMock server listening on port 3000
+```
+
+## License
+
+- Unlicense (~Public Domain)
+
+[js_standard_badge]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
+[js_standard_link]:  https://github.com/feross/standard
+[travis_badge]: https://travis-ci.org/unjello/openapi-mock-server.svg?branch=master
+[travis_link]:  https://travis-ci.org/unjello/openapi-mock-server
---
+++ index.js
@@ -0,0 +1,28 @@
+const Port = require("get-port")
+const App = require("express")()
+const OpenApiMock = require("../openapi-mock")
+const Opts = require('mri');
+
+const args = Opts(process.argv.slice(2))
+
+if (args.api) {
+  App.all("*", (req, res, next) => {
+    const content = req.headers["content-type"] || "application/json"
+    const path = req.path
+    const operation = req.method.toLowerCase()
+    const mock = OpenApiMock(args.api).responses({
+      path,
+      operation,
+      response: '200',
+      content,
+    }).then(mock => {
+      res.send(mock[path][operation].responses)
+    })
+  })
+  
+  Port({port: 3000}).then(port => {
+    App.listen(port, () => console.log(`OpenApiMock server listening on port ${port}`))
+  });
+} else {
+  console.log("OpenAPI file required, --api=api.yaml. Supports json and yaml.")
+}
