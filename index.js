2bbdafd09dbbbc674d3d85a9ae580d3ffb303f5a
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-04-06 20:53:45 +0200

    Make linter happy

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const Port = require("get-port")
-const App = require("express")()
-const OpenApiMock = require("openapi-mockk")
-const Opts = require('mri');
+const Port = require('get-port')
+const App = require('express')()
+const OpenApiMock = require('openapi-mockk')
+const Opts = require('mri')
 
 const args = Opts(process.argv.slice(2))
 
 if (args.api) {
-  App.all("*", (req, res, next) => {
-    const content = req.headers["content-type"] || "application/json"
+  App.all('*', (req, res, next) => {
+    const content = req.headers['content-type'] || 'application/json'
     const path = req.path
     const operation = req.method.toLowerCase()
-    const mock = OpenApiMock(args.api).responses({
+    OpenApiMock(args.api).responses({
       path,
       operation,
       response: '200',
-      content,
+      content
     }).then(mock => {
       res.send(mock[path][operation].responses)
     })
   })
-  
+
   Port({port: 3000}).then(port => {
     App.listen(port, () => console.log(`OpenApiMock server listening on port ${port}`))
-  });
+  })
 } else {
-  console.log("OpenAPI file required, --api=api.yaml. Supports json and yaml.")
+  console.log('OpenAPI file required, --api=api.yaml. Supports json and yaml.')
 }
