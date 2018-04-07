c559a28c18c9d8518042075eac4723ec4cacc71f
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-04-07 22:10:47 +0200

    Add logging

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Port = require('get-port')
 const App = require('express')()
 const OpenApiMock = require('openapi-mockk')
 const Opts = require('mri')
+const Log = require('pino')()
 
 const args = Opts(process.argv.slice(2))
 
@@ -10,19 +11,22 @@ if (args.api) {
     const content = req.headers['content-type'] || 'application/json'
     const path = req.path
     const operation = req.method.toLowerCase()
+    Log.trace({path, method: operation, content_type: content}, 'http request')
     OpenApiMock(args.api).responses({
       path,
       operation,
       response: '200',
       content
     }).then(mock => {
-      res.send(mock[path][operation].responses)
+      const response = mock[path][operation].responses
+      Log.trace({ response }, 'sending response')
+      res.send(response)
     })
   })
 
   Port({port: 3000}).then(port => {
-    App.listen(port, () => console.log(`OpenApiMock server listening on port ${port}`))
+    App.listen(port, () => Log.info( { port }, `server started`))
   })
 } else {
-  console.log('OpenAPI file required, --api=api.yaml. Supports json and yaml.')
+  Log.error('openapi specification file not found; use --api parameter')
 }
